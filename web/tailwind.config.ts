61c8b625e5ef5982c7cf96cb830d12a3a2621ba2
Hiep Nguyen <nguyenhieptech@gmail.com>
2025-01-02 13:49:56 +0700

    feat: animate collapsible up and down

--- web/tailwind.config.ts
+++ web/tailwind.config.ts
@@ -66,7 +66,7 @@ export default {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
-      transitionTimingFunction: {
+      transitionDuration: {
         "400": "400ms",
       },
       keyframes: {
@@ -86,10 +86,28 @@ export default {
             height: "0",
           },
         },
+        "collapsible-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-collapsible-content-height)",
+          },
+        },
+        "collapsible-up": {
+          from: {
+            height: "var(--radix-collapsible-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "collapsible-down": "collapsible-down 0.25s ease-in-out",
+        "collapsible-up": "collapsible-up 0.25s ease-in-out",
       },
     },
   },
