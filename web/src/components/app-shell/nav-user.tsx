b271a49ccf79926e95c366dba3867e3dcbfe7f92
Hiep Nguyen <nguyenhieptech@gmail.com>
2025-01-06 13:54:49 +0700

    feat: update border radius

--- web/src/components/app-shell/nav-user.tsx
+++ web/src/components/app-shell/nav-user.tsx
@@ -45,9 +45,9 @@ export function NavUser({
               size="lg"
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
-              <Avatar className="h-8 w-8 rounded-lg">
+              <Avatar className="h-8 w-8 rounded-md">
                 <AvatarImage src={user.avatar} alt={user.name} />
-                <AvatarFallback className="rounded-lg">LN</AvatarFallback>
+                <AvatarFallback className="rounded-md">LN</AvatarFallback>
               </Avatar>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-semibold">{user.name}</span>
@@ -64,9 +64,9 @@ export function NavUser({
           >
             <DropdownMenuLabel className="p-0 font-normal">
               <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                <Avatar className="h-8 w-8 rounded-lg">
+                <Avatar className="h-8 w-8 rounded-md">
                   <AvatarImage src={user.avatar} alt={user.name} />
-                  <AvatarFallback className="rounded-lg">LN</AvatarFallback>
+                  <AvatarFallback className="rounded-md">LN</AvatarFallback>
                 </Avatar>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">{user.name}</span>
