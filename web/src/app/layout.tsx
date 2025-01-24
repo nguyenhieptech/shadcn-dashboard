fb0404ff907a2fc2323c7660f5838f873655baca
Hiep Nguyen <nguyenhieptech@gmail.com>
2025-01-25 01:47:30 +0700

    feat: suppress hydration warning

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${font.className} antialiased`}>
         <AppProvider>{children}</AppProvider>
       </body>
--- web/src/components/providers/theme-provider.tsx
+++ web/src/components/providers/theme-provider.tsx
@@ -1,19 +1,13 @@
 // https://ui.shadcn.com/docs/dark-mode/next#create-a-theme-provider
-// TODO: https://github.com/shadcn-ui/ui/pull/6006/files
 
 "use client";
 
-import { type ThemeProviderProps } from "next-themes";
-import dynamic from "next/dynamic";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import * as React from "react";
 
-const NextThemesProvider = dynamic(
-  () => import("next-themes").then((e) => e.ThemeProvider),
-  {
-    ssr: false,
-  }
-);
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({
+  children,
+  ...props
+}: React.ComponentProps<typeof NextThemesProvider>) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
